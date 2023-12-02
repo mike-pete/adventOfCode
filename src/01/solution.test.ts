604d186a7258cf240f7a084ffcc933f836bd0c0a
Mike Peterson <mike@lemonshell.com>
2023-12-02 13:13:19 -0700

    finish day one

--- src/01/solution.test.ts
+++ src/01/solution.test.ts
@@ -1,6 +1,18 @@
-import {expect, test} from '@jest/globals';
+import { expect, test } from '@jest/globals'
 import { getCalibrationValueFromLine } from './solution'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(getCalibrationValueFromLine('')).toBe(true);
-});
+test('1abc2 -> 12', () => {
+	expect(getCalibrationValueFromLine('1abc2')).toBe(12)
+})
+
+test('pqr3stu8vwx -> 38', () => {
+	expect(getCalibrationValueFromLine('pqr3stu8vwx')).toBe(38)
+})
+
+test('a1b2c3d4e5f -> 15', () => {
+	expect(getCalibrationValueFromLine('a1b2c3d4e5f')).toBe(15)
+})
+
+test('treb7uchet -> 77', () => {
+	expect(getCalibrationValueFromLine('treb7uchet')).toBe(77)
+})
--- src/01/solution.ts
+++ src/01/solution.ts
@@ -1,23 +1,41 @@
-import fs from 'fs';
-import readline from 'readline';
+import fs from 'fs'
+import path from 'path'
+import readline from 'readline'
 
+export const getCalibrationValueFromLine = (line: string) => {
+	let firstNumber = ''
+	let lastNumber = ''
 
-export const getCalibrationValueFromLine = (line:string) => {
-    let firstNumber: string, lastNumber: string
-    return true
+	for (let char of line) {
+		const isNumber = !Number.isNaN(parseInt(char))
+		if (isNumber) {
+			if (!firstNumber) {
+				firstNumber = char
+			}
+			lastNumber = char
+		}
+	}
+	console.log(firstNumber)
+	return parseInt(firstNumber + lastNumber)
 }
 
 const sumCalibrationValues = async () => {
-    let sum = 0
-    const file = readline.createInterface({
-        input: fs.createReadStream('input.txt'),
-        output: process.stdout,
-        terminal: false
-    })
-    
-    file.on('line', (line) => {
-        getCalibrationValueFromLine(line)
-    });
+	let sum = 0
+
+	const file = readline.createInterface({
+		input: fs.createReadStream(path.resolve(__dirname, 'input.txt')),
+		output: process.stdout,
+		terminal: false,
+	})
+
+	file.on('line', (line) => {
+		const value = getCalibrationValueFromLine(line)
+		sum += value
+	})
+
+	file.on('close', () => {
+		console.log(sum)
+	})
 }
 
-
+sumCalibrationValues()
